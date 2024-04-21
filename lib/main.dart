28496bd1fc664e5173f40674de7b1d70ce9d965e
kdrbipul <kdrbipul@gmail.com>
2024-04-21 11:33:42 +0600

    google map setup

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,9 @@ import 'package:firebase_project/firebase_massaging.dart';
 import 'package:firebase_project/movie_list_screen.dart';
 import 'package:firebase_project/tennis_live_score_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import 'firebase_options.dart';
-
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -25,8 +25,32 @@ class MoviesApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       // home: MovieListScreen(),
-      home: TennisLiveScoreScreen(docId: '7',),
+      // home: TennisLiveScoreScreen(docId: '7',),
+      home: HomeScreen(),
     );
   }
 }
 
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Google Map'),
+      ),
+      body: const GoogleMap(
+        zoomControlsEnabled: true,
+        initialCameraPosition: CameraPosition(
+          target: LatLng(24.227928802754363, 90.16462487803568),
+        ),
+      ),
+    );
+  }
+}
