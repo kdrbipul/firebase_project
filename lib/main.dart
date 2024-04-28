1dd6002ee33671ecde3abceff871cfc41e64a228
kdrbipul <kdrbipul@gmail.com>
2024-04-28 10:39:05 +0600

    some change

--- lib/main.dart
+++ lib/main.dart
@@ -45,25 +45,77 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: const Text('Google Map'),
       ),
-      body:  GoogleMap(
+      body: GoogleMap(
         zoomControlsEnabled: true,
         initialCameraPosition: const CameraPosition(
           target: LatLng(24.227928802754363, 90.16462487803568),
-          zoom: 10,
+          zoom: 17,
           bearing: 90,
           tilt: 90,
         ),
         myLocationEnabled: true,
         myLocationButtonEnabled: true,
-        onTap: (LatLng latLng){
+        onTap: (LatLng latLng) {
           print('tap on map : Latlng $latLng');
         },
-        onLongPress: (LatLng latLng){
+        onLongPress: (LatLng latLng) {
           print('on long press : Latlng $latLng');
         },
-        compassEnabled: true,
-        zoomGesturesEnabled: false,
-        liteModeEnabled: true,
+        /*compassEnabled: true,
+        zoomGesturesEnabled: true,
+        liteModeEnabled: true,*/
+        markers: {
+          Marker(
+            markerId: const MarkerId('my-new-resturant'),
+            position: const LatLng(23.86114041722582, 90.00732421875003),
+            infoWindow: const InfoWindow(title: 'Montaj Hotel & Resturant'),
+            icon:
+                BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
+            // draggable: true,
+            draggable: false,
+            flat: false,
+          ),
+          Marker(
+            markerId: const MarkerId('my-new-resturant'),
+            position: const LatLng(23.863715587282346, 89.98641595244408),
+            infoWindow: const InfoWindow(title: 'Montaj Hotel & Resturant'),
+            icon: BitmapDescriptor.defaultMarkerWithHue(
+                BitmapDescriptor.hueYellow),
+            // draggable: true,
+            draggable: false,
+            flat: false,
+          ),
+          Marker(
+            markerId: const MarkerId('my-new-resturant'),
+            position: const LatLng(23.843769151308564, 90.01542337238789),
+            infoWindow: const InfoWindow(title: 'Montaj Hotel & Resturant'),
+            icon:
+                BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueCyan),
+            // draggable: true,
+            draggable: false,
+            flat: false,
+          ),
+          Marker(
+            markerId: const MarkerId('my-new-resturant'),
+            position: const LatLng(23.882288286866036, 90.03135770559311),
+            infoWindow: const InfoWindow(title: 'Montaj Hotel & Resturant'),
+            icon:
+                BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRose),
+            draggable: true,
+            // draggable: false,
+            flat: false,
+          ),
+        },
+        circles: {
+          Circle(
+            circleId: const CircleId('new resturant'),
+            center:const LatLng(24.227928802754363, 90.16462487803568),
+            radius: 50,
+            strokeColor: Colors.orange,
+            strokeWidth: 3,
+            fillColor: Colors.red.withOpacity(0.15),
+          ),
+        },
       ),
     );
   }
